069778189fda19e5402ba8880af3428c4d43e8b3
codeshootah <vnukovg@gmail.com>
2019-10-25 16:27:18 +0300

    navabar component

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <template>
   <div id="app">
-
-    <div>
-      <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-container>
-          <b-navbar-brand to="/">Store</b-navbar-brand>
-
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
-              <b-nav-item to="/">Home</b-nav-item>
-              <b-nav-item to="/about">About</b-nav-item>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-container>
-      </b-navbar>
-    </div>
-
+    <NavBar />
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import NavBar from '@/components/NavBar.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    NavBar
   }
 }
+</script>
+
+<style lang="scss">
+
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container>
+    <h1>About</h1>
+  </b-container>
 </template>
