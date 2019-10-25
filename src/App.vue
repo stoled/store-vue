ae501e90d4a797b9ba802415847d46d2f3971270
codeshootah <vnukovg@gmail.com>
2019-10-25 15:30:05 +0300

    add bootstrap and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-container>
+          <b-navbar-brand to="/">Store</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item to="/">Home</b-nav-item>
+              <b-nav-item to="/about">About</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
     </div>
+
     <router-view/>
   </div>
 </template>
